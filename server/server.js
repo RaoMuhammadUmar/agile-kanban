5ccd118134c57dbb71a7e084830f8f5789cf3d4b
RaoMuhammadUmar <raomuhammadomer@gmail.com>
2026-08-20 20:09:34 +0500

    Prepare Express API for Vercel

--- server/server.js
+++ server/server.js
@@ -94,10 +94,12 @@ app.use((err, req, res, next) => {
   });
 });
 
-const PORT = process.env.PORT || 5000;
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(
-    `Agile Kanban API listening on http://localhost:${PORT}`
-  );
-});
+  app.listen(PORT, () => {
+    console.log(`Agile Kanban API listening on http://localhost:${PORT}`);
+  });
+}
+
+export default app;
